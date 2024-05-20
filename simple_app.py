f9066af44e9e6cc530536199bc39bc80e18c864a
GavFeng <gavfeng@uw.edu>
2024-05-20 05:49:53 +0000

    a

--- simple_app.py
+++ simple_app.py
@@ -80,6 +80,9 @@ def timeout_mode():
     else:
         st.session_state.messages.append({"role": "assistant", "content": "Automatic timeout mode disabled."})
         st.session_state.timeout_mode = False
+        
+    if st.session_state.timeout_mode:
+        check_inactivity()
 
 
 st.sidebar.button('Clear chat history', on_click=clear_chat_history)
@@ -105,8 +108,6 @@ def check_inactivity():
 
     # Trigger the script to rerun after a short delay
     st.experimental_set_query_params(_timeout_refreshed=current_time)
-
-
 
 @st.cache_resource(show_spinner=False)
 def get_tokenizer():
