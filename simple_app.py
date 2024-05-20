71821ba12a7c82e4cd3c23020060e98c70d4e924
GavFeng <gavfeng@uw.edu>
2024-05-20 05:50:39 +0000

    a

--- simple_app.py
+++ simple_app.py
@@ -81,7 +81,7 @@ def timeout_mode():
         st.session_state.messages.append({"role": "assistant", "content": "Automatic timeout mode disabled."})
         st.session_state.timeout_mode = False
         
-    if st.session_state.timeout_mode:
+    while st.session_state.timeout_mode:
         check_inactivity()
 
 
